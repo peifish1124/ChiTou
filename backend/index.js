a35c79a2c339366378fc2681fe64ab391718a0a2
Hsin-Yun Lai <lai1029700@gmail.com>
2023-08-15 11:27:36 +0800

    Feature: Signup function

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,21 @@
-// package require
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
-const port = 3000;
+//const port = 3000;  // for main server
+const port = 3011; // for development only
+
+// routes require
+const user_route = require('./server/routes/user_route');
+
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use('/api/users', user_route);
 
 app.get('/', (req, res) => {
-    res.send('Hello, My Server!');
+    res.send('<h1 style="text-align: center; padding: 20px;">Hello, My Server!</h1>');
 });
 
 app.listen(port, () => {
--- backend/server/models/user_model.js
+++ backend/server/models/user_model.js
@@ -1,2 +1,44 @@
-module.exports = {
-}
+const poolConnection = require('../../utils/db_connection');
+
+exports.nameExist = async (name) => {
+  const connection = await poolConnection();
+  const query = `
+    SELECT COUNT(id)
+    FROM users 
+    WHERE name = ?
+    `;
+  
+  try {
+    const existingUserCount = await connection.query(query, [name]);
+    return existingUserCount[0][0]['COUNT(id)'] > 0;
+  } catch (err) {
+    console.error(err);
+    return null;
+  } finally {
+    connection.release();
+  }
+};
+
+exports.createUser = async (name, password) => {
+  const connection = await poolConnection();
+  const query = `
+    INSERT INTO users (name, password)
+    VALUES (?, ?)
+    `;
+
+  try {
+    const [result] = await connection.query(query, [name, password]);
+    if(result.affectedRows === 1) {
+      return {
+        id: result.insertId,
+        name: name,
+      }
+    }
+    return null;
+  }   catch (err) {
+    console.error(err);
+    return null;
+  } finally {
+    connection.release();
+  }
+};
