c22f5609cb5d822cbc89f93501336e7db5a0afe5
brownrice-02 <vickylee.g123@gmail.com>
2023-08-17 09:16:41 +0800

    frontend branch

--- backend/server/routes/user_route.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const userController = require('../controllers/user_controller');
-
-router.post('/signup', userController.signup);
-router.post('/signin', userController.signin);
-router.get('/search', userController.authorization, userController.search);
-
-module.exports = router;
