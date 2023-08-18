842d9e8afa7868515fee595c1e9399fbd52d200b
brownrice-02 <vickylee.g123@gmail.com>
2023-08-18 11:58:52 +0800

    feat(login/signup): add login/signup page

---
+++ frontend/src/app/login/page.js
@@ -0,0 +1,47 @@
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+
+"use client";
+
+import { useState } from "react";
+import styles from "@/styles/css-modules/LoginSignupPage.module.scss";
+import nav from "@/styles/css-modules/nav.module.scss";
+import LoginSignupPage from "./LoginSignupPage";
+
+export default function Login() {
+  const [isLoginMode, setLoginMode] = useState(true); // true 為登入頁面
+
+  // 註冊登入頁面切換用
+  const handleSwitchMode = () => {
+    setLoginMode(!isLoginMode);
+  };
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.main} style={{ minHeight: "64.4%" }}>
+        <div className={styles.leftside}>
+          <h1 className={nav.logo} style={{ fontSize: "3.2rem" }}>
+            ChiTou
+          </h1>
+          <h2>會員{isLoginMode ? "登入" : "註冊"}</h2>
+          <div className={styles.enterBox}>
+            <LoginSignupPage
+              isLoginMode={isLoginMode}
+              handleSwitchMode={handleSwitchMode}
+            />
+          </div>
+          <div className={styles.bottomtext}>
+            <span className={styles.lefttext}>尚未成為會員？</span>
+            <span className={styles.righttext} onClick={handleSwitchMode}>
+              會員{isLoginMode ? "註冊" : "登入"}
+            </span>
+          </div>
+        </div>
+        <div className={styles.rightside} />
+      </div>
+      <div className={styles.copyright}>
+        關於我們 · 隱私權條款 · Cookie 條款 · © 2023 CanChu, Inc.
+      </div>
+    </div>
+  );
+}
---
+++ frontend/src/hooks/useLogin.js
@@ -0,0 +1,69 @@
+// useLogin.js
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import { setCookie } from "nookies";
+import Swal from "sweetalert2";
+import axiosInstance from "@/utils/axiosInstance";
+
+const useLogin = () => {
+  const router = useRouter();
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState();
+
+  const handleLogin = (userData) => {
+    setIsLoading(true); // 開始 API 請求時設置 isLoading 為 true
+    axiosInstance
+      .post("/users/signin", userData)
+      .then((response) => {
+        if (response.status === 200) {
+          console.log("登入成功");
+          setCookie(null, "access_token", response.data.data.access_token, {
+            maxAge: 30 * 24 * 60 * 60,
+            // path: "/",
+          });
+          setCookie(null, "user_id", response.data.data.user.id, {
+            maxAge: 30 * 24 * 60 * 60,
+            // path: "/",
+          });
+          router.push("/"); // 進入首頁
+        } else {
+          console.log("登入失敗");
+          setError(response.data.message);
+          if (response.status >= 500 && response.status < 600) {
+            alert("伺服器出現問題，請稍後再試或通知我們的工程團隊。");
+            // Swal.fire({
+            //   icon: "error",
+            //   title: "登入失敗",
+            //   text: response.data.message,
+            // });
+          }
+        }
+      })
+      .catch((error) => {
+        // console.log("登入失敗2", error.response.data.error);
+        // console.log("登入失敗2", error.response.status);
+        setError(error.message);
+        if (error.response.status >= 500 && error.response.status < 600) {
+          Swal.fire({
+            icon: "error",
+            title: "伺服器出現問題",
+            text: "請稍後再試或通知我們的工程團隊。",
+          });
+        }
+        if (error.response.status >= 400 && error.response.status < 500) {
+          Swal.fire({
+            icon: "error",
+            title: "登入失敗",
+            text: error.response.data.error,
+          });
+        }
+      })
+      .finally(() => {
+        setIsLoading(false); // 無論請求成功或失敗，設置 isLoading 為 false
+      });
+  };
+
+  return { handleLogin, isLoading, error };
+};
+
+export default useLogin;
