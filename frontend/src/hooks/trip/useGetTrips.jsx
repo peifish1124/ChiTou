f4a9fb10964e04a631d214f27b402d50e49b7944
Ade Liu <dylan920901@gmail.com>
2023-08-21 10:39:31 +0800

    fix(eslint): useGetTrips import order

--- frontend/src/hooks/trip/useGetTrips.jsx
+++ frontend/src/hooks/trip/useGetTrips.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
-import axiosAuth from "@/api/axiosAuth";
 import Swal from "sweetalert2";
+import axiosAuth from "@/api/axiosAuth";
 
 export default function useGetTrips() {
   const [trips, setTrips] = useState([]);
