ac68f95f23473d67c99082d81c3a28d558895c63
Pei-Yu Chen <b08705026@ntu.edu.tw>
2023-08-23 10:09:42 +0800

    line bot welcome message

--- backend/utils/line_bot.js
+++ backend/utils/line_bot.js
@@ -1,12 +1,21 @@
 module.exports = {
   handleEvent: async (event, client) => {
+    if (event.type === 'join' && event.source.type === 'group') {
+      const welcomeMessage = {
+        type: 'text',
+        text: '歡迎加入本群組！',
+      };
+
+      await client.replyMessage(event.replyToken, welcomeMessage);
+    }
+
     if (event.type === 'message' && event.message.type === 'text') {
       const messageText = event.message.text;
       if (messageText === 'Hello') {
-      await client.replyMessage(event.replyToken, {
-          type: 'text',
-          text: 'Hello, how can I help you?',
-      });
+        await client.replyMessage(event.replyToken, {
+            type: 'text',
+            text: 'Hello, how can I help you?',
+        });
       }
     }
   }
